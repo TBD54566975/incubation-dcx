d29a7ebe53fa6174a658d17f430b406445cfd0dc
Bnonni <bnonni@formfree.com>
2024-05-31 18:41:47 -0400

    massive reorg - DCX

---
+++ src/schemas/index.ts
@@ -0,0 +1,4 @@
+export { schema as presentationSchema } from './presentation.js';
+export { schema as responseSchema } from './response.js';
+export { schema as invoiceSchema } from './invoice.js';
+export { schema as manifestSchema } from './manifest.js';
---
+++ src/utils/time.ts
@@ -0,0 +1,36 @@
+/**
+ * Time related utilities.
+ */
+export class Time {
+  /**
+   * Sleeps for the desired duration
+   * @param durationInMillisecond the desired amount of sleep time
+   * @returns when the provided duration has passed
+   */
+  public static async sleep(durationInMillisecond: number = 0): Promise<void> {
+    await new Promise((resolve) => setTimeout(resolve, durationInMillisecond));
+  }
+
+  /**
+   * Retries fn() after sleep  for the desired duration if retries > 0
+   * @param fn the function to retry
+   * @param retries the number of retries
+   * @param retryIntervalMs the duration to sleep between retries
+   * @throws error if retries <= 0
+   * @returns the result of fn()
+   */
+  public static async retry<T>(
+    fn: () => Promise<T> | T,
+    { retries, retryIntervalMs }: { retries: number; retryIntervalMs: number },
+  ): Promise<T> {
+    try {
+      return await fn();
+    } catch (error) {
+      if (retries <= 0) {
+        throw error;
+      }
+      await Time.sleep(retryIntervalMs);
+      return this.retry(fn, { retries: retries - 1, retryIntervalMs });
+    }
+  }
+}
