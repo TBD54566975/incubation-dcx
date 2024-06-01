a638aefc8c93b27c833cae0775f3207b68f1a63f
Bnonni <bnonni@formfree.com>
2024-06-01 19:46:56 -0400

    fix failing build

---
+++ test/index.ts
@@ -0,0 +1,10 @@
+import * as Lab from '@hapi/lab';
+import { expect } from '@hapi/code';
+
+const lab = Lab.script();
+const { describe, it, after, afterEach, before, beforeEach } = lab;
+export { lab };
+
+describe('API', () => {
+
+});
