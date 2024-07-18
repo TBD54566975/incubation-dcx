39acbae7e4b1f8da5cc0f5b822d19799ab0c5c1e
Bnonni <bnonni@formfree.com>
2024-07-18 19:05:35 -0400

    update schemas

--- src/schemas/invoice.ts
+++ src/schemas/invoice.ts
@@ -1,6 +1,8 @@
+export type InvoiceSchema = typeof schema;
+
 export const schema = {
+  $id        : 'https://formfree.github.io/.well-known/schemas/dvcx/invoice.json',
   $schema    : 'http://json-schema.org/draft-07/schema#',
-  $id        : 'https://dcx.io/protocol/credential-issuance/schemas/invoice',
   type       : 'object',
   properties : {
     url: {
