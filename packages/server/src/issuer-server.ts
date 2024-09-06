4724b410d5d517fca5caf173ea436e7fb320a0b8
Bnonni <bnonni@formfree.com>
2024-09-06 13:46:58 -0400

    fixing manifest fitlering in issuer server; adding trycatch; fixing ms sleep; create new method for sleeping

--- packages/server/src/issuer-server.ts
+++ packages/server/src/issuer-server.ts
@@ -1,4 +1,5 @@
 import {
+  CredentialApplicationVP,
   CredentialManifest,
   DcxPath,
   FileSystem,
@@ -72,80 +73,84 @@ export class IssuerServer implements IServer {
    * Listens for incoming records from the DWN
    */
   public async listen(params: SleepTime = { ms: '2m' }): Promise<void> {
-    await this.sync();
-
     this.server.listening = true;
-    Logger.log('IssuerServer listening ...');
-
     const milliseconds = ms(params.ms);
-
     const CURSOR = this.issuer.config.cursorFile;
-
     let cursor = await FileSystem.readToJson(CURSOR);
     const pagination = Objects.isEmpty(cursor) ? {} : { cursor };
 
+    Logger.log('IssuerServer listening ...');
     while (this.server.listening) {
-      const { records = [] } = await this.issuer.web5.dwn.records.query({
-        from    : this.issuer.agent.agentDid.uri,
-        message : {
-          pagination,
-          filter : { protocol: issuer.protocol },
-        },
-      });
+      try {
 
-      const recordIds = records.map((record: Record) => record.id);
+        await this.sync();
 
-      const recordReads: Record[] = await Promise.all(
-        recordIds.map(async (recordId: string) => {
-          const { record }: { record: Record } = await this.issuer.web5.dwn.records.read({
-            from    : this.issuer.agent.agentDid.uri,
-            message : {
-              filter : {
-                recordId,
+        const { records = [] } = await this.issuer.web5.dwn.records.query({
+          from    : this.issuer.agent.agentDid.uri,
+          message : {
+            pagination,
+            filter : { protocol: issuer.protocol },
+          },
+        });
+
+        const recordIds = records.map((record: Record) => record.id);
+
+        const reads: Record[] = await Promise.all(
+          recordIds.map(async (recordId: string) => {
+            const { record }: { record: Record } = await this.issuer.web5.dwn.records.read({
+              from    : this.issuer.agent.agentDid.uri,
+              message : {
+                filter : {
+                  recordId,
+                },
               },
-            },
-          });
-          return record;
-        }),
-      );
+            });
+            return record;
+          }),
+        );
 
-      if (this.server.testing) {
-        Logger.log('Test Complete! Stopping DCX server ...');
-        this.stop();
-      }
-
-      if (!recordReads.length) {
-        Logger.log('No records found!', recordReads.length);
-        await Time.sleep(milliseconds);
-      }
-
-      for (const record of recordReads) {
-        if (record.protocolPath === 'application') {
-          const data = await record.data.json();
-          Logger.debug(`Found application with id: ${record.id}`, data);
-
-          const manifest = this.issuer.config.manifests.find(
-            (manifest: CredentialManifest) =>
-              manifest.id.toLowerCase() === data.vpDataModel.credential_application.manifest_id,
-          );
-
-          if (!manifest) {
-            Logger.error(`Manifest not found for application with id: ${record.id}`);
-            continue;
-          }
-
-          const { status } = await this.issuer.processApplicationRecord(
-            {
-              record,
-              manifest,
-              providerId : manifest.issuer.id,
-            }
-          );
-          Logger.debug(`Processed application with id: ${record.id}`, status);
+        if (this.server.testing) {
+          Logger.log('Test Complete! Stopping DCX server ...');
+          this.stop();
         }
+
+        if (!reads.length) {
+          Logger.log('No records read!', reads.length);
+          this.sleepServer(milliseconds);
+        }
+
+        for (const read of reads) {
+          if (read.protocolPath === 'application') {
+            const data: CredentialApplicationVP = await read.data.json();
+            Logger.debug(`Found application with id: ${read.id}`, data);
+
+            const manifest = this.issuer.config.manifests.find(
+              (manifest: CredentialManifest) => manifest.id.toLowerCase() === data.credential_application.manifest_id.toLowerCase(),
+            );
+
+            if (!manifest) {
+              Logger.error(`Manifest not found for application with id: ${read.id}`);
+              continue;
+            }
+
+            const { status } = await this.issuer.processApplicationRecord({
+              manifest,
+              record     : read,
+              providerId : manifest.issuer.id,
+            });
+            Logger.debug(`Processed application with id: ${read.id}`, status);
+          }
+        }
+        this.sleepServer(milliseconds);
+      } catch (error) {
+        Logger.error(error);
       }
-      await Time.sleep(20000);
     }
+  }
+
+  private async sleepServer(milliseconds: number): Promise<void> {
+    Logger.info(`Server sleeping for ${milliseconds}ms ...`);
+    await Time.sleep(milliseconds);
   }
 
   /**
@@ -165,11 +170,10 @@ export class IssuerServer implements IServer {
       if (!this.issuer.status.initialized) {
         await this.issuer.initialize();
       }
-
       if(!this.issuer.status.setup) {
         await this.issuer.setup();
       }
-
+      await this.sync();
       await this.listen();
     } catch (error: any) {
       Logger.error(error);
